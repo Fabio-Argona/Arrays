c0e1eadffe7f907b013400d84ee15a483c7f6507
Bowie <128233610+Fabio-Argona@users.noreply.github.com>
2023-08-07 20:53:04 -0300

    exemplos de Arrys para explicar

---
+++ atualizando-lista.js
@@ -0,0 +1,13 @@
+const nomes = [
+    'joão',
+    'Ana',
+    'Caio',
+    'Lara', 
+    'Marjorie',
+    'Leo'
+];
+
+nomes.splice(1,2, "Rodrigo");
+
+
+console.log(nomes);
---
+++ lista-duas-dimensoes.js
@@ -0,0 +1,7 @@
+const alunos = ["João","Juliana","Ana","Caio"]; 
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunosEMedias = [alunos, medias];
+
+console.log(`A Aluna da posição 1 da lista de Alunos é: ${listaDeAlunosEMedias[0][1]}`);
+console.log(`A nota da Aluna é: ${listaDeAlunosEMedias[1][1]}`);
